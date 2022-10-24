c627b84e7c0f07c1c58041996857814a237e0956
AsiburSumon <sumonasif68@gmail.com>
2022-10-24 16:43:08 +0600

    news

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Home = () => {
+    const AllNews = useLoaderData();
     return (
         <div>
-            <h2>This is Home</h2>
+            <h2>Dragon News Home: {AllNews.length}</h2>
+            {
+                AllNews.map (news => <NewsSummaryCard key={news._id} news={news}>
+                </NewsSummaryCard>)
+            }
         </div>
     );
 };
